0386f4ac79fc1655e43b6d567810d5b802c457a9
anetkabagetka <anebol01@easv365.dk>
2026-03-23 21:46:21 +0100

    created hero area/image

--- wp-content/themes/hotel-theme/index.php
+++ wp-content/themes/hotel-theme/index.php
@@ -1,4 +1,7 @@
 <?php get_header() ?>
-
-
+    <?php if(have_posts()) : ?>
+        <?php while(have_posts()) : the_post() ?>
+            <?php get_template_part('template-parts/index', 'hero'); ?>
+        <?php endwhile; ?>
+    <?php endif; ?>
 <?php get_footer() ?>
---
+++ wp-content/themes/hotel-theme/template-parts/index-hero.php
@@ -0,0 +1,5 @@
+<?php
+$hotelPhoto = get_field('hotel_photo');
+?>
+
+<div class="hero" style="background-image: url(<?= $hotelPhoto['url']; ?>);"></div>
