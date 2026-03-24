e595d6d8c349be3dd33a41307831901db75519f5
anetkabagetka <anebol01@easv365.dk>
2026-03-24 14:45:30 +0100

    styling and coding welcome

--- wp-content/themes/hotel-theme/template-parts/index-hero.php
+++ wp-content/themes/hotel-theme/template-parts/index-hero.php
@@ -1,5 +1,13 @@
 <?php
 $hotelPhoto = get_field('hotel_photo');
+$welcomeSection = get_field('welcome_section');
+$welcomeMessage = $welcomeSection['welcome_message'];
+$hotelDescription = $welcomeSection['hotel_description'];
 ?>
 
-<div class="hero" style="background-image: url(<?= $hotelPhoto['url']; ?>);"></div>
+<div class="hero" style="background-image: url(<?= $hotelPhoto['url']; ?>);">
+
+<h1><?= $welcomeMessage ?> </h1>
+<p><?= $hotelDescription ?></p>
+
+</div>
