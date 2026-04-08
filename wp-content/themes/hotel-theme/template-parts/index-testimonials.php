57519318e327b61dcad1cb53592e0038d23b64cd
trexlerviri-svg <trexlerviri@gmail.com>
2026-04-08 12:06:45 +0200

    Testimonials

--- wp-content/themes/hotel-theme/template-parts/index-testimonials.php
+++ wp-content/themes/hotel-theme/template-parts/index-testimonials.php
@@ -1,15 +1,16 @@
 <?php
-$testimonials =get_field("testimonial");?>
+$testimonials = get_field("testimonial");
+?>
 <div id="testimonials">
-<h2>Testimonials<h2>
-<?php foreach($testimonials as $testimonial):?>
-
-<?php
-
-$quote=get_the_title($testimonial->ID);
-$author=get_field("author",$testimonial->ID);
- ?>
- <div class="testimonial">
-<div class="quote">"<?= $quote;?></div>
-<div class="author">"<?=$author?></div>
-<?php endforeach;?>
+<h2>Feedbacks from Our Guests</h2>
+<?php foreach($testimonials as $testimonial) : ?>
+    <?php
+    $quote = get_the_title($testimonial->ID);
+    $author = get_field("author", $testimonial->ID);
+    ?>
+    <div class="testimonial">
+        <div class="quote">"<?php echo $quote; ?>"</div>
+        <div class="author"><?php echo $author; ?></div>
+    </div>
+<?php endforeach; ?>
+</div>
